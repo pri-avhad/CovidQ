d3e02e75b67ae31bacd8c653e205ce4524056a2f
Priyanka Avhad <64562764+pri-avhad@users.noreply.github.com>
2021-03-21 04:11:12 +0530

    Add files via upload

---
+++ oximeter/oximeter.ino
@@ -0,0 +1,24 @@
+#include <Wire.h>
+#include "MAX30100_PulseOximeter.h"
+
+PulseOximeter pox;
+uint32_t lastReport = 0;
+int oxi = 0;
+
+void setup()
+{
+    Serial.begin(9600);
+    if (!pox.begin()) {
+        Serial.println("FAILED");
+        for(;;);
+    } 
+     pox.setIRLedCurrent(MAX30100_LED_CURR_7_6MA);
+}
+
+void loop()
+{
+    pox.update();
+        oxi = pox.getSpO2();
+        if(oxi > 1 && oxi <= 100)
+        Serial.println(oxi);
+}
